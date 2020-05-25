8e12ef1a3bd2b2f54c2e82913330e02df1507f15
fsalvador91 <f.salvador91@gmail.com>
2020-05-25 17:10:45 -0300

    commit inicial

---
+++ WebApp.API/WebApp.TesteIntegracao/Scenarios/ValuesTest.cs
@@ -0,0 +1,48 @@
+﻿using System.Net;
+using FluentAssertions;
+using System.Threading.Tasks;
+using WebApp.TesteIntegracao.Fixtures;
+using Xunit;
+
+namespace WebApp.TesteIntegracao.Scenarios
+{
+    public class ValuesTest
+    {
+        private readonly TestContext _testContext;
+        public ValuesTest()
+        {
+            _testContext = new TestContext();
+        }
+
+        [Fact]
+        public async Task GetRetornaOk()
+        {
+            var response = await _testContext.Client.GetAsync("/api/values");
+            response.EnsureSuccessStatusCode();
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task GetByIDRetornaOK()
+        {
+            var response = await _testContext.Client.GetAsync("/api/values/5");
+            response.EnsureSuccessStatusCode();
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task GetByIdInvalidoRetornaBadRequest()
+        {
+            var response = await _testContext.Client.GetAsync("/api/values/XXX");
+            response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        }
+
+        [Fact]
+        public async Task GetByIdVerificaContentType()
+        {
+            var response = await _testContext.Client.GetAsync("/api/values/5");
+            response.EnsureSuccessStatusCode();
+            response.Content.Headers.ContentType.ToString().Should().Be("text/plain; charset=utf-8");
+        }
+    }
+}
